f92539a77bf00cdbceeaae91d5d0683be4941cbe
Ben Diegel <b.diegelg@gmail.com>
2017-07-16 21:33:02 -0400

    I use this file to test things

---
+++ Playground.py
@@ -0,0 +1,51 @@
+__author__ = 'BenDiegel'
+
+
+
+#def test():
+ #   global choice
+  #  global choice2
+
+   # print ("Please choose a character!")
+   # print ("1. Warrior")
+    #print ("2. Mage")
+ #   print ("3. Rouge")
+  #  choice = input()
+  #  print ("Now choose a weapon")
+   # print ("1. Sword")
+    #print ("2. Wand")
+    #print ("3. Dagger")
+    #choice2 = input()
+
+
+#print (choice)
+#print (choice2)
+
+#playerclass = ['warrior', 'ninja', 'pirate']
+
+#Class = playerclass[0]
+
+#import subprocess
+
+
+#subprocess.Popen([r"C:\Program Files (x86)\Pivot Animator\pivot.exe"])
+
+
+
+#class test():
+    #test1 = "This is a local test"
+
+#print(test.test1)
+
+
+userName = ['Ben']
+
+def foo(userName):
+    if not userName:
+        print("User: ")
+    else:
+        print(userName[0] + ": ")
+
+def start():
+    raw_input(foo(userName))
+start()
